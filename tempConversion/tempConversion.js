c16a0512312721987daf56846014d564456be8b9
borason <bzilla1982@gmail.com>
2020-03-06 20:44:11 -0700

    finish tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,14 @@
-const ftoc = function() {
+var ftoc = function(fahrenheit) {
+  var celsius = ((fahrenheit - 32) * 5) / 9;
+  return Math.round(celsius * 10) / 10;
+};
 
-}
-
-const ctof = function() {
-
-}
+var ctof = function(celsius) {
+  var fahrenheit = (celsius * 9) / 5 + 32;
+  return Math.round(fahrenheit * 10) / 10;
+};
 
 module.exports = {
   ftoc,
   ctof
-}
+};
